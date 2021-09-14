c202a94e7210d5a7e95afbf63d1845773ffba7c0
jae2tree <jae2tree@gmail.com>
2021-09-14 19:57:43 +0900

    target delete

--- first_curi/target/rls/debug/deps/first_curi-ef5790f5bd361984.d
+++
@@ -1,5 +0,0 @@
-c:\Users\JAE_MYUNG\Projects\rust\first_curi\target\rls\debug\deps\first_curi-ef5790f5bd361984.rmeta: src\main.rs
-
-c:\Users\JAE_MYUNG\Projects\rust\first_curi\target\rls\debug\deps\first_curi-ef5790f5bd361984.d: src\main.rs
-
-src\main.rs:
